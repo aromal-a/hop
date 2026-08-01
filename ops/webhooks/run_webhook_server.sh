67c356f901df7709297a9f5882b15cb369a4cb76
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-08-01 17:01:48 +0530

    Update run_webhook_server.sh

--- ops/webhooks/run_webhook_server.sh
+++ ops/webhooks/run_webhook_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /usr/local/bin/gws -port=4665 -method=POST -path=/postreceive -secret=$WEBHOOK_SECRET -command=$(pwd)/webhook_postreceive.sh
+/bin/ssh -port=95 -method = Testing -techpath = /restVI -secret =$Webhook_secrets -method.e[post-post.ssh] : Double(2:8)
